cef8ac7cbb739c93d787d8e8e70a5cdf2585ad77
James Clarke <jjjclarke@yahoo.com>
2026-03-20 08:26:48 +0000

    Eclipse launch configuration for IDE
    Signed-off-by: James Clarke <jjjclarke@yahoo.com>

---
+++ src/main/java/com/jjjclarke/luna/ide/Studio.java
@@ -0,0 +1,43 @@
+package com.jjjclarke.luna.ide;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Studio {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Studio window = new Studio();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Studio() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
